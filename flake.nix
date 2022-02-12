a4c0efaee9ccca0a729273b82744ca78680e9648
Daniel Firth <dan.firth@homotopic.tech>
2022-02-12 18:33:49 +0000

    Fix library functions

--- flake.nix
+++ flake.nix
@@ -6,14 +6,12 @@
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let pkgs = import nixpkgs { inherit system; };
+          lib = import ./lib.nix { inherit pkgs; };
       in
       {
-      checks =
-        with pkgs;
-        with import ./lib.nix { inherit pkgs; };
-       {
-        hlint = runCommand "hlint" { meta = { description = "Run hlint"; }; } "cd ${self}; ${hlint}/bin/hlint | tee $out";
-        nixpkgs-fmt = porcelainOrDie "nixpkgs-fmt" { meta = { description = "Run nixpkgs-fmt"; }; } "find . -name '*.nix' | xargs -I x ${nixpkgs-fmt}/bin/nixpkgs-fmt $x;" "Nixpkgs-fmt errors, run nixpkgs-fmt";
-      };
-    });
+        linters = lib;
+        checks = {
+          nixpkgs-fmt = lib.nixpkgs-fmt ./.;
+        };
+      });
 }
--- lib.nix
+++ lib.nix
@@ -1,6 +1,9 @@
 { pkgs }:
 rec {
+
   porcelainOrDieScript = cmd: advice: pkgs.writers.writeBashBin "lint" ''
+    ls
+    echo "Running ${cmd}"
     ${cmd}
     if [ -z "$(git status --porcelain)" ]; then
       echo "OK"
@@ -9,7 +12,13 @@ rec {
       exit 1
     fi
     '';
-  porcelainOrDie = name: attrs: cmd: advice: pkgs.runCommand name attrs ''
+
+  porcelainOrDie = src: name: cmd: advice: pkgs.stdenv.mkDerivation {
+    name = name;
+    src = src;
+    meta = { description = "Linting check: ${name}"; };
+    dontBuild = true;
+    installPhase = ''
     PATH="$PATH:${pkgs.git}/bin"
     export GIT_AUTHOR_NAME="nobody"
     export EMAIL="no@body.com"
@@ -18,4 +27,19 @@ rec {
     git commit -m "init"
     ${porcelainOrDieScript cmd advice}/bin/lint | tee $out
     '';
+   };
+
+   hlint = src: pkgs.stdenv.mkDerivation {
+     name = "hlint";
+     src = src;
+     meta = { description = "Run hlint"; };
+     dontBuild = true;
+     installPhase = ''
+       ${pkgs.hlint}/bin/hlint | tee $out
+     '';
+   };
+
+   nixpkgs-fmt = src: porcelainOrDie src "nixpkgs-fmt"
+                         "find . -name '*.nix' | xargs ${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt;"
+                         "Found errors with nixpkgs-fmt, try running 'find . -name \'*.nix\' | xargs nixpkgs-fmt'";
 }
