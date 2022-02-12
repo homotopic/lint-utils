07701942544fc597448e9f777a86d649ea821ee3
Daniel Firth <dan.firth@homotopic.tech>
2022-02-12 17:15:11 +0000

    Do properly

--- flake.nix
+++ flake.nix
@@ -1,10 +1,14 @@
 {
   inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = {
-    checks = {
-      hlint = runCommand "hlint" { meta = { description = "Run hlint"; }; } "cd ${self}; ${hlint}/bin/hlint";
-    };
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+      {
+      checks = with import nixpkgs { inherit system; };
+      {
+        hlint = runCommand "hlint" { meta = { description = "Run hlint"; }; } "cd ${self}; ${hlint}/bin/hlint";
+      };
+    });
 }
