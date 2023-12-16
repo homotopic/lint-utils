d5d14d581b0f5ddff50d63ba59d1c9f264269e1a
Mason Mackaman <masondeanm@aol.com>
2023-12-15 21:02:08 -0500

    Add linting for RFC 166
    https://github.com/NixOS/rfcs/pull/166

--- flake.nix
+++ flake.nix
@@ -1,12 +1,16 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    nixfmt-rfc166.url = "github:piegamesde/nixfmt/rfc101-style";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixfmt-rfc166, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (_: _: { nixfmt-rfc166 = nixfmt-rfc166.packages.${system}.default; }) ];
+        };
         writers = pkgs.callPackage ./pkgs/writers.nix { inherit (pkgs.stdenv) mkDerivation; inherit (pkgs.writers) writeBashBin; };
         linters = with pkgs.stdenv; pkgs.callPackage ./pkgs/linters.nix { inherit writers; };
       in
