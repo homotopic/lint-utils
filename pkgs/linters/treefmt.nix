84f0661623ff265c4cd558af8a237b917674749d
Sebastian Nagel <ch1bo@users.noreply.github.com>
2025-02-24 12:56:06 +0100

    Use --no-cache for treefmt
    This avoids writing the cache as seemingly newer versions of `treefmt` do:
    
    ```
    Error: failed to open cache: could not resolve local path for the cache: could not create any of the following paths: [/homeless-shelter/.cache/treefmt/eval-cache]
    ```

--- pkgs/linters/treefmt.nix
+++ pkgs/linters/treefmt.nix
@@ -6,6 +6,6 @@
   meta = { description = "Run treefmt"; };
   dontBuild = true;
   installPhase = ''
-    ${treefmt}/bin/treefmt --fail-on-change | tee $out
+    ${treefmt}/bin/treefmt --fail-on-change --no-cache | tee $out
   '';
 }
